3ab28de55cc2848bcc9567a582e133fed14fcb36
Christopher Hertel <mail@christopher-hertel.de>
2023-11-27 21:36:39 +0100

    adding deployment

---
+++ deploy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+set('repository', 'git@github.com:chr-hertel/symfonycon-bot-2023.git');
+set('composer_options', '--no-dev --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction --no-scripts');
+set('console_options', '--no-interaction --env=prod');
+set('shared_files', [
+    '.env.local',
+    'var/data.db',
+]);
+
+// Hosts
+host('stof.fail')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '/var/www/symfonycon-bot-2023');
+
+// Tasks
+task('build', function () {
+    cd('{{release_path}}');
+    run('{{bin/console}} dotenv:dump {{console_options}}');
+});
+
+after('deploy:cache:clear', 'build');
+after('deploy:cache:clear', 'database:migrate');
+after('deploy:failed', 'deploy:unlock');
